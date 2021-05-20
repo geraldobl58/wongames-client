326986eeca248bc2b114bd032792fcb338575d16
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-20 20:22:20 -0300

    feat: Finalizing card game component

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -2,8 +2,11 @@ import * as S from './styles'
 
 import {
   FavoriteBorder,
-  AddShoppingCart
+  AddShoppingCart,
+  Favorite
 } from '@styled-icons/material-outlined'
+
+import Ribbon, { RibbonColors, RibbonSizes } from '../Ribbon'
 
 import Button from '../Button'
 
@@ -12,24 +15,53 @@ export type GameCardProps = {
   developer: string
   img: string
   price: string
+  promotionalPrice?: string
+  favorite?: boolean
+  ribbon?: React.ReactNode
+  ribbonColor?: RibbonColors
+  ribbonSize?: RibbonSizes
+  onFav?: () => void
 }
 
-const GameCard = ({ title, developer, img, price }: GameCardProps) => (
+const GameCard = ({
+  title,
+  developer,
+  img,
+  price,
+  promotionalPrice,
+  favorite = false,
+  ribbon,
+  ribbonColor = 'primary',
+  ribbonSize = 'small',
+  onFav
+}: GameCardProps) => (
   <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon color={ribbonColor} size={ribbonSize}>
+        {ribbon}
+      </Ribbon>
+    )}
     <S.ImageBox>
       <img src={img} alt={title} />
     </S.ImageBox>
-    <S.Info>
-      <S.Title>{title}</S.Title>
-      <S.Developer>{developer}</S.Developer>
-    </S.Info>
-    <S.FavButton role="button">
-      <FavoriteBorder aria-label="Add to Wishlist" />
-    </S.FavButton>
-    <S.BuyBox>
-      <S.Price>{price}</S.Price>
-      <Button icon={<AddShoppingCart />} size="small" />
-    </S.BuyBox>
+    <S.Content>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton onClick={onFav} role="button">
+        {favorite ? (
+          <Favorite aria-label="Remove from Whishlist " />
+        ) : (
+          <FavoriteBorder aria-label="Add to Wishlist" />
+        )}
+      </S.FavButton>
+      <S.BuyBox>
+        {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
+        <S.Price>{promotionalPrice || price}</S.Price>
+        <Button icon={<AddShoppingCart />} size="small" />
+      </S.BuyBox>
+    </S.Content>
   </S.Wrapper>
 )
 
