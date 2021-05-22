31a349e8a2c41a68d87ec806fc4d29dc25441656
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-22 17:26:18 -0300

    feat: Game card slider adding CSS and tests

--- src/components/GameCard/styles.ts
+++ src/components/GameCard/styles.ts
@@ -25,6 +25,10 @@ export const ImageBox = styled.a`
   );
   background-size: 80rem 14rem;
   animation: placeholderShimmer 1s linear infinite forwards;
+
+  img {
+    width: 100%;
+  }
   @keyframes placeholderShimmer {
     0% {
       background-position: -40rem 0;
