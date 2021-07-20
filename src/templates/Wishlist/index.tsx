b6ecd324ae9b7fe0989aabbc8b2d2867f290b0ac
Geraldo Luiz <geraldobl58@gmail.com>
2021-07-20 14:24:56 -0300

    :sparkles: Creating the wishlist page and template

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,19 @@
+import Base from 'templates/Base'
+
+import { Container } from 'components/Container'
+
+import Heading from 'components/Heading'
+
+import * as S from './styles'
+
+const Wishlist = () => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Wishlist
+      </Heading>
+    </Container>
+  </Base>
+)
+
+export default Wishlist
