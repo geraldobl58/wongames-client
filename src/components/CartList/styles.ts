993687515944b8b8ffae02cc416fdd6d6c12477b
Geraldo Luiz <geraldobl58@gmail.com>
2021-07-29 15:31:03 -0300

    :hammer: Creating option to show empty cart

--- src/components/CartList/styles.ts
+++ src/components/CartList/styles.ts
@@ -2,12 +2,38 @@ import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 import { tint } from 'polished'
 
-export const Wrapper = styled.main`
-  ${({ theme }) => css`
+import * as EmptyStyles from '../Empty/styles'
+
+type WrapperProps = {
+  isEmpty: boolean
+}
+
+export const Wrapper = styled.div<WrapperProps>`
+  ${({ theme, isEmpty }) => css`
     background: ${theme.colors.white};
     display: flex;
     flex-direction: column;
     align-self: start;
+
+    ${isEmpty &&
+    css`
+      ${EmptyStyles.Wrapper} {
+        padding-bottom: ${theme.spacings.medium};
+      }
+
+      ${EmptyStyles.Image} {
+        max-width: 20rem;
+      }
+
+      ${EmptyStyles.Title} {
+        font-size: ${theme.font.sizes.large};
+      }
+
+      ${EmptyStyles.Description} {
+        color: ${theme.colors.black};
+        font-size: ${theme.font.sizes.medium};
+      }
+    `}
   `}
 `
 
