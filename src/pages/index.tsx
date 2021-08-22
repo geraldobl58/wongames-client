d6fa728380c765a40e6af14168089cbc4b77a67b
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-22 10:07:58 -0300

    :sparkles: Fetching Banner Data on Client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,37 @@ import Home, { HomeTemplateProps } from '../templates/Home'
 import bannerMock from '../components/BannerSlider/mock'
 import gamesMock from '../components/GameCardSlider/mock'
 import highlightMock from '../components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
+  console.log(data.banners)
+
   return {
     props: {
-      banners: bannerMock,
+      revalidate: 10,
+      banners: data.banners.map((banner) => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text || null,
+          ribbonColor: banner.ribbon.color || null,
+          ribbonSize: banner.ribbon.size || null
+        })
+      })),
+
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
