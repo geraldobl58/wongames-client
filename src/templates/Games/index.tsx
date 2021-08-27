e0b7015b75e6dbc4dc3370ed897e44d3e0775f26
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-27 16:09:16 -0300

    :sparkles: Using useQuery on the client side

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,3 +1,7 @@
+import { useQuery } from '@apollo/client'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+import { QUERY_GAMES } from 'graphql/queries/games'
+
 import Base from 'templates/Base'
 
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
@@ -13,7 +17,12 @@ export type GamesTemplateProps = {
   filterItems: ItemProps[]
 }
 
-const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
+const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { data, loading } = useQuery<QueryGames, QueryGamesVariables>(
+    QUERY_GAMES,
+    { variables: { limit: 9 } }
+  )
+
   const handleFilter = () => {
     return
   }
@@ -29,8 +38,15 @@ const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
 
         <section>
           <Grid>
-            {games.map((item) => (
-              <GameCard key={item.title} {...item} />
+            {data?.games.map((game) => (
+              <GameCard
+                key={game.slug}
+                title={game.name}
+                slug={game.slug}
+                developer={game.developers[0].name}
+                img={`http://localhost:1337${game.cover!.url}`}
+                price={game.price}
+              />
             ))}
           </Grid>
 
