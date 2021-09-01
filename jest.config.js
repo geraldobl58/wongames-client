9d020471d92a379c709ae1eb8883c49b0859892d
Geraldo Luiz <geraldobl58@gmail.com>
2021-09-01 09:43:56 -0300

    :hammer: Removing files from coverage

--- jest.config.js
+++ jest.config.js
@@ -7,8 +7,11 @@ module.exports = {
     '!src/**/stories.tsx',
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
+    '!src/utils/apolloCache.ts',
     '!src/utils/apollo.ts',
-    '!src/types/**/*.d.ts'
+    '!src/types/**/*.d.ts',
+    '!src/graphql/**/*.ts',
+    '!src/**/mock.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
