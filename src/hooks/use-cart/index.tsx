d6aaf23623c0931dead121e82ee990c29e3347a0
Geraldo Luiz <geraldobl58@gmail.com>
2021-09-01 15:32:28 -0300

    :sparkles: Creating cartMapper

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useState } from 'react'
 import { useQueryGames } from 'graphql/queries/games'
 import { getStorageItem } from 'utils/localStorage'
 import formatPrice from 'utils/format-price'
+import { cartMapper } from 'utils/mappers'
 
 const CART_KEY = 'cartItems'
 
@@ -51,12 +52,7 @@ const CartProvider = ({ children }: CartProviderProps) => {
   return (
     <CartContext.Provider
       value={{
-        items: data?.games.map((game) => ({
-          id: game.id,
-          img: `http://localhost:1337${game.cover?.url}`,
-          price: formatPrice(game.price),
-          title: game.name
-        }))
+        items: cartMapper(data?.games)
       }}
     >
       {children}
