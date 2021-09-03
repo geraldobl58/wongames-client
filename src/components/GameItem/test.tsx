74ef51dc7a54ae9fa9841bc325501d75f16ef43e
Geraldo Luiz <geraldobl58@gmail.com>
2021-09-03 12:05:40 -0300

    :sparkles: Adding remove item from cart in Game

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -1,8 +1,11 @@
+import userEvent from '@testing-library/user-event'
+import { CartContextDefaultValues } from 'hooks/use-cart'
 import { render, screen } from 'utils/test-utils'
 
 import GameItem from '.'
 
 const props = {
+  id: '1',
   img: 'https://source.unsplash.com/user/willianjusten/151x70',
   title: 'Red Dead Redemption 2',
   price: 'R$ 215,00'
@@ -12,16 +15,31 @@ describe('<GameItem />', () => {
   it('should render the item', () => {
     render(<GameItem {...props} />)
 
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
     expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
       'src',
       props.img
     )
 
-    expect(
-      screen.getByRole('heading', { name: props.title })
-    ).toBeInTheDocument()
-
     expect(screen.getByText('R$ 215,00')).toBeInTheDocument()
+  })
+
+  it('should render remove if the item is inside the cart and call remove', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => true,
+      removeFromCart: jest.fn()
+    }
+    render(<GameItem {...props} />, { cartProviderProps })
+
+    const removeLink = screen.getByText(/remove/i)
+    expect(removeLink).toBeInTheDocument()
+
+    userEvent.click(removeLink)
+    expect(cartProviderProps.removeFromCart).toHaveBeenCalledWith('1')
   })
 
   it('should render the item with download link', () => {
@@ -37,7 +55,7 @@ describe('<GameItem />', () => {
   it('should render the payment info', () => {
     const paymentInfo = {
       flag: 'mastercard',
-      img: '/img/cards/master-card.png',
+      img: '/img/master-card.png',
       number: '**** **** **** 4326',
       purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
     }
