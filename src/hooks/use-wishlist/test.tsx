2e72db67b9be74de4de3acb08a0603d17244b985
Geraldo Luiz <geraldobl58@gmail.com>
2021-10-04 17:40:12 -0300

    :sparkles: Creating mocks and initial test framework for useWishlist

---
+++ src/hooks/use-wishlist/test.tsx
@@ -0,0 +1,15 @@
+import { MockedProvider } from '@apollo/client/testing'
+import { renderHook } from '@testing-library/react-hooks'
+import { useWishlist, WishlistProvider } from '.'
+
+describe('useWishlist', () => {
+  it('should return wishlist item', () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[]}>
+        <WishlistProvider>{children}</WishlistProvider>
+      </MockedProvider>
+    )
+
+    const { result } = renderHook(() => useWishlist(), { wrapper })
+  })
+})
