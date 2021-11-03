925dd17046eaab08dddcd883fa261fc116db58de
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-03 17:35:34 -0300

    :hammer: Optimizing Highlight to use next/image

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import * as S from './styles'
 
 import Button from '../Button'
@@ -21,8 +23,13 @@ const Highlight = ({
   buttonLink,
   alignment = 'right'
 }: HighlightProps) => (
-  <S.Wrapper alignment={alignment} backgroundImage={backgroundImage}>
-    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
+  <S.Wrapper alignment={alignment}>
+    <Image src={backgroundImage} alt={title} layout="fill" />
+    {!!floatImage && (
+      <S.FloatImageWrapper>
+        <Image src={floatImage} alt={title} width={400} height={300} />
+      </S.FloatImageWrapper>
+    )}
     <S.Content>
       <S.Title>{title}</S.Title>
       <S.Subtitle>{subtitle}</S.Subtitle>
