d5049d3cfa8eb396d5d709fee59a8ff7f00a7cf1
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-04 16:45:29 -0300

    :hammer: Setting NextSEO default

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.localhost:3000',
+    site_name: 'Won Games',
+    title: 'Won Games'
+  },
+  twitter: {
+    handle: '@johndoe',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
