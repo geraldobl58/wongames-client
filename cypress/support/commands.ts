7d533cf9ef53afa106e3c3b3f579f5b388546f01
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-18 15:21:36 -0300

    :rotating_light: Refactoring: creating custom commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -28,3 +28,21 @@
 import '@testing-library/cypress/add-commands';
 
 Cypress.Commands.add('google', () => cy.visit('https://google.com'))
+
+Cypress.Commands.add('shouldRenderBanners', () => {
+  cy.get('.slick-slider').within(() => {
+    cy.findByRole('heading', { name: /Cyberpunk 2077 is an action role-playing video game developed and published by CD Projekt/i })
+    cy.findByRole('link', { name: /buy now/i })
+
+    cy.get('.slick-dots > :nth-child(2) > button').click()
+    cy.wait(500)
+
+    cy.findByRole('heading', { name: /Biomutant é um RPG de ação desenvolvido pelo desenvolvedor sueco Experiment 101 e publicado pela THQ Nordic./i })
+    cy.findByRole('link', { name: /buy now/i })
+
+    cy.get('.slick-dots > :nth-child(3) > button').click()
+
+    cy.findByRole('heading', { name: /You awake in a nightmarish carnival and watch a golden-haired woman hurl herself down a bottomless well for your sake./i })
+    cy.findByRole('link', { name: /buy now/i })
+  })
+})
