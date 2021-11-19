d07df388c20c5d0ab415e7907828a75eab0b4a7c
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-19 12:05:41 -0300

    :rotating_light: Game Details and Showcase

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -22,5 +22,22 @@ describe('Game Page', () => {
     })
 
     cy.getByDataCy('content').children().should('have.length.at.least', 2)
+
+    cy.getByDataCy('game-details').within(() => {
+      cy.findByRole('heading', { name: /game details/i }).should('exist')
+      cy.findByRole('heading', { name: /developer/i }).should('exist')
+      cy.findByRole('heading', { name: /release date/i }).should('exist')
+      cy.findByRole('heading', { name: /platforms/i }).should('exist')
+      cy.findByRole('heading', { name: /publisher/i }).should('exist')
+      cy.findByRole('heading', { name: /rating/i }).should('exist')
+      cy.findByRole('heading', { name: /genres/i }).should('exist')
+
+      cy.findByText(/experiment 101/i).should('exist')
+      cy.findByText(/may 23, 2021/i).should('exist')
+      cy.findByRole('img', { name: /windows/i }).should('exist')
+      cy.findByText(/thq nordic gmbh/i).should('exist')
+      cy.findByText(/free/i).should('exist')
+      cy.findByText('Action / Adventure / Open World').should('exist')
+    })
   })
 })
