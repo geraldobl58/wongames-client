1cf4ba750c071dfd05d4c69523faf7b02775cde2
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-19 14:06:59 -0300

    :rotating_light: Adding game to cart via game page

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -1,9 +1,11 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Game Page', () => {
-  it('should render game page sections', () => {
+  before(() => {
     cy.visit('/game/biomutant')
+  })
 
+  it('should render game page sections', () => {
     cy.wait(10000);
 
     cy.getByDataCy('game-info').within(() => {
@@ -38,6 +40,23 @@ describe('Game Page', () => {
       cy.findByText(/thq nordic gmbh/i).should('exist')
       cy.findByText(/free/i).should('exist')
       cy.findByText('Action / Adventure / Open World').should('exist')
+    })
+  })
+
+  it('should add/remove game in cart', () => {
+    cy.wait(10000);
+
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('button', { name: /add to cart/i }).click()
+      cy.findByRole('button', { name: /remove from cart/i }).should('exist')
+    })
+
+    cy.findAllByLabelText(/cart items/i)
+      .first().should('have.text', 1)
+      .click()
+
+    cy.getByDataCy('cart-list').within(() => {
+      cy.findByRole('heading', { name: /biomutant/i }).should('exist')
     })
   })
 })
