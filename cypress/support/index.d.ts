2d83ab02bf73c38f5bce9c8e82a6249e44b61375
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-22 15:55:32 -0300

    :rotating_light: Creating custom SignUp command

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,6 +1,12 @@
 // load type definations from Cypress module
 /// <reference types="cypress" />
 
+type User = {
+  username: string
+  email: string
+  password: string
+}
+
 type ShowcaseAttributes = {
   name: string
   highlight?: boolean
@@ -18,6 +24,12 @@ declare namespace Cypress {
      * @example cy.google()
      */
     google(): Chainable<Window>
+
+    /**
+     * Custom command to sign in
+     * @example cy.signUp({ username: 'john', email: 'johndoe@email.com', 'password: '123' })
+     */
+     signUp(user: User): Chainable<Element>
 
      /**
      * Custom command to get element by data-cy values
