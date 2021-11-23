fd44c986041384de5e1535f9b04f6ac64116edcd
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-23 12:19:47 -0300

    :rotating_light: Using Intercept for next-auth

--- cypress/e2e/reset-password.ts
+++ cypress/e2e/reset-password.ts
@@ -38,4 +38,32 @@ describe('Reset Password', () => {
 
     cy.findByText(/Incorrect code provided/i).should('exist')
   })
+
+  it.only('should fill the input and redirect to the home page with the user signed in', () => {
+    cy.intercept('POST', '**/auth/reset-password', {
+      statusCode: 200,
+      body: { user: { email: 'cypress@email.com' } }
+    })
+
+    cy.intercept('POST', '**/auth/callback/credentials*', {
+      statusCode: 200,
+      body: { user: { email: 'cypress@email.com' } }
+    })
+
+    cy.intercept('GET', '**/auth/session', {
+      statusCode: 200,
+      body: { user: { name: 'cypress', email: 'cypress@email.com' } }
+    })
+
+    cy.visit('/reset-password?code=valid_token')
+
+    cy.findAllByPlaceholderText(/^password/i).type('123')
+    cy.findAllByPlaceholderText(/confirm password/i).type('123')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+    cy.findByText(/cypress/i).should('exist')
+
+  })
 })
