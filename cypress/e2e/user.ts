d806c4d36cc342eafa5d52a645a7b00934aa760f
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-23 09:21:28 -0300

    :rotating_light: Testing for SignIn

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -3,13 +3,27 @@
 import { createUser } from '../support/generate'
 
 describe('User', () => {
-  it('should sign up', () => {
+  it.skip('should sign up', () => {
     const user = createUser()
     cy.visit('/sign-up')
     cy.signUp(user)
 
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(user.username).should('exist')
+  })
 
+  it('should sign in and sign out', () => {
+    cy.wait(10000);
+    cy.visit('/sign-in')
+
+    cy.findByPlaceholderText(/email/i).type('e2e@wongames.com')
+    cy.findByPlaceholderText(/^password/i).type('123456')
+    cy.findByRole('button', { name: /sign in now/i }).click()
+
+    cy.findByText(/cypress/i).should('exist').click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+    cy.findByText(/cypress/i).should('not.exist')
   })
 });
