80b2823b0e90cf6fe0ba1f75875641cccf7257f4
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-24 14:52:42 -0300

    :rotating_light: Creating full flow for paid games

--- cypress/e2e/checkout.ts
+++ cypress/e2e/checkout.ts
@@ -53,6 +53,53 @@ describe('Checkout Page', () => {
   })
 
   describe('Paid Games', () => {
+    before(() => {
+      user = createUser()
+    })
 
+    it('should buy paid games', () => {
+      cy.visit('/sign-up')
+      cy.signUp(user)
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+      cy.findByRole('link', { name: /explore/i }).click()
+      cy.url().should('eq', `${Cypress.config().baseUrl}/games`)
+
+      cy.findByText(/Highest to lowest/i).click()
+      cy.url().should('contain', 'sort=price%3Adesc')
+
+      cy.addToCartByIndex(0)
+
+      cy.findAllByLabelText(/cart items/i)
+        .first()
+        .should('have.text', 1)
+        .click()
+
+      cy.getByDataCy('cart-list').within(() => {
+        cy.findByText(/buy it now/i).click()
+      })
+
+      cy.findByRole('button', { name: /buy now/i }).should('have.attr', 'disabled')
+
+      cy.fillElementsInput('cardNumber', '4242424242424242')
+      cy.fillElementsInput('cardExpiry', '1040')
+      cy.fillElementsInput('cardCvc', '103')
+
+      cy.findByRole('button', { name: /buy now/i }).click()
+
+      cy.url().should('eq', `${Cypress.config().baseUrl}/success`)
+
+      cy.findByRole('heading', { name: /Your purchase was successful!/i }).should('exist')
+    })
+
+    it('should show games in order page', () => {
+      cy.visit('/profile/orders')
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/orders`)
+
+      cy.signIn(user.email, user.password)
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/orders`)
+
+      cy.getByDataCy('game-item').should('have.length', 1)
+    })
   })
 })
